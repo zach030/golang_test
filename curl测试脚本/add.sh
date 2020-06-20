1931d34dfd270304a2de7f7f92559228eed4d8ba
Zach <zachchou016@gmail.com>
2020-06-20 21:12:29 +0800

    添加curl

---
+++ curl测试脚本/add.sh
@@ -0,0 +1,5 @@
+curl --location --request POST 'http://127.0.0.1:8080/add' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'typeid=111' \
+--data-urlencode 'name=百事可乐' \
+--data-urlencode 'parentid=106'
---
+++ curl测试脚本/search.sh
@@ -0,0 +1,4 @@
+curl http://localhost:8080/search/100
+
+curl --location --request GET 'http://127.0.0.1:8080/search/100' \
+--header 'Content-Type: application/x-www-form-urlencoded'
